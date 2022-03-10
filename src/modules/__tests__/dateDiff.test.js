368b272a1c46cb5bfd7968fdd6cc7b9f0179ca4c
JΛVI <hello@soyjavi.com>
2022-03-10 13:10:34 +0700

    Improved dateDiff()

--- src/modules/__tests__/dateDiff.test.js
+++ src/modules/__tests__/dateDiff.test.js
@@ -2,7 +2,7 @@ import { dateDiff } from '../dateDiff';
 
 const DATE = new Date(1980, 3, 10, 5, 15, 45);
 const NEXT_2_DAYS = new Date(1980, 3, 10 + 2, 5, 15, 45);
-const NEXT_45_DAYS = new Date(1980, 3, 10 + 45, 5, 15, 45);
+const NEXT_25_DAYS = new Date(1980, 3, 10 + 25, 5, 15, 45);
 const NEXT_500_DAYS = new Date(1980, 3, 10 + 500, 5, 15, 45);
 
 describe('@modules/dateDiff', () => {
@@ -11,17 +11,19 @@ describe('@modules/dateDiff', () => {
   });
 
   test('Naive use', () => {
-    expect(dateDiff(DATE, NEXT_2_DAYS)).toEqual({ days: 2, months: 0, years: 0 });
-    expect(dateDiff(DATE, NEXT_45_DAYS)).toEqual({ days: 45, months: 1, years: 0 });
-    expect(dateDiff(DATE, NEXT_500_DAYS)).toEqual({ days: 500, months: 16, years: 1 });
+    expect(dateDiff(DATE, NEXT_2_DAYS)).toEqual({ years: 0, months: 0, days: 2 });
+    expect(dateDiff(DATE, NEXT_25_DAYS)).toEqual({ years: 0, months: 1, days: 25 });
+    expect(dateDiff(DATE, NEXT_500_DAYS)).toEqual({ years: 1, months: 16, days: 500 });
   });
 
   test('Incorrect use', () => {
-    expect(dateDiff()).toEqual({ days: undefined, months: undefined, years: undefined });
-    expect(dateDiff(DATE)).toEqual({ days: undefined, months: undefined, years: undefined });
-    expect(dateDiff(undefined, DATE)).toEqual({ days: undefined, months: undefined, years: undefined });
-    expect(dateDiff(NEXT_2_DAYS, DATE)).toEqual({ days: undefined, months: undefined, years: undefined });
-    expect(dateDiff(DATE, '1980/04/10')).toEqual({ days: undefined, months: undefined, years: undefined });
-    expect(dateDiff('1980/04/10', DATE)).toEqual({ days: undefined, months: undefined, years: undefined });
+    const error = { years: undefined, months: undefined, days: undefined };
+
+    expect(dateDiff()).toEqual(error);
+    expect(dateDiff(DATE)).toEqual(error);
+    expect(dateDiff(undefined, DATE)).toEqual(error);
+    expect(dateDiff(NEXT_2_DAYS, DATE)).toEqual(error);
+    expect(dateDiff(DATE, '1980/04/10')).toEqual(error);
+    expect(dateDiff('1980/04/10', DATE)).toEqual(error);
   });
 });
--- src/modules/dateDiff.js
+++ src/modules/dateDiff.js
@@ -1,20 +1,19 @@
 import { isDate } from './isDate';
 
 const MS_DAY = 1000 * 60 * 60 * 24;
-const MS_MONTH = MS_DAY * 30;
-const MS_YEAR = MS_DAY * 365;
 
 export const dateDiff = (from, to) => {
-  let days;
-  let months;
   let years;
+  let months;
+  let days;
 
   if (isDate(from) && isDate(to) && from < to) {
     const diff = to.getTime() - from.getTime();
+
+    years = to.getFullYear() - from.getFullYear();
+    months = years * 12 + to.getMonth() - from.getMonth();
     days = parseInt(diff / MS_DAY, 10);
-    months = parseInt(diff / MS_MONTH, 10);
-    years = parseInt(diff / MS_YEAR, 10);
   }
 
-  return { days, months, years };
+  return { years, months, days };
 };
